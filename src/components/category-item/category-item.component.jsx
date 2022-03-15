72f86beb6faa6e1df5b4ce02a251a20dc33854c5
satish <satish11rr@gmail.com>
2022-03-15 10:37:16 -0500

    basic structure of app is designed

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,16 @@
+import "./category-item.styles.scss";
+
+const CategoryItem = ({category}) => {
+  const {title,imageUrl,id} = category
+  return (
+    <div key={id} className="category-container">
+      <div className="background-image" style={{backgroundImage : `url(${imageUrl})`}} />
+      <div className="category-body-container">
+        <h2>{title.toUpperCase()}</h2>
+        <p>Shop now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,17 @@
+import './directory.styles.scss'
+import CategoryItem from '../category-item/category-item.component';
+const Directory = ({ categories }) => {
+  
+   return (
+      <div className='categories-container'> 
+         {
+            categories.map((category) => (
+               <CategoryItem category={category} />
+            ))
+        }
+      </div>
+   )
+}
+
+
+export default Directory;
